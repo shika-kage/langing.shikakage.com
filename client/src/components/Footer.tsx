aac326e9b1f028e7d034ccc77521173c80d05763
shika-kage <shika.kage.official@gmail.com>
2021-11-21 18:55:59 +0200

    v0.0.1 - ch

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,41 +6,46 @@ import { ITranslateProps } from '@/interfaces/Components';
 import Documentation from "@assets/docs/documentation.pdf";
 
 export const Footer = withTranslation()((props: ITranslateProps) => (
-    <footer>
-        <div className='container'>
-            <div className='pre-footer__content'>
-                <ul>
-                    <li>
-                        <Link to='/'>{ props.t('menu_item_home') }</Link>
-                    </li>
-                    <li>
-                        <a href={Documentation}>{ props.t('footer_link_litepaper') }</a>
-                    </li>
-                    <li>
-                        <Link to="/">{ props.t('footer_link_poocoin_chart') }</Link>
-                    </li>
-                    <li>
-                        <Link to="/">{ props.t('footer_link_docs') }</Link>
-                    </li>
-                </ul>
-                <ul className='footer__links'>
-                    <li>
-                        <Link to="/">{ props.t('footer_link_platform_skg') }</Link>
-                    </li>
-                    <li>
-                        <Link to="/">{ props.t('footer_link_buy_sk') }</Link>
-                    </li>
-                    <li>
-                        <Link to="/">{ props.t('footer_link_privacy_policy') }</Link>
-                    </li>
-                </ul>
-                <div className='footer__community'>
-                    <h4>{ props.t('footer_community_title') }</h4>
-                    <p>{ props.t('footer_community_desc') }</p>
-                    <SocialNetworks />
+    <>
+        <footer>
+            <div className='container'>
+                <div className='pre-footer__content'>
+                    <ul>
+                        <li>
+                            <Link to='/'>{ props.t('menu_item_home') }</Link>
+                        </li>
+                        <li>
+                            <a href={Documentation}>{ props.t('footer_link_litepaper') }</a>
+                        </li>
+                        <li>
+                            <Link to="/">{ props.t('footer_link_poocoin_chart') }</Link>
+                        </li>
+                        <li>
+                            <Link to="/">{ props.t('footer_link_docs') }</Link>
+                        </li>
+                    </ul>
+                    <ul className='footer__links'>
+                        <li>
+                            <Link to="/">{ props.t('footer_link_platform_skg') }</Link>
+                        </li>
+                        <li>
+                            <Link to="/">{ props.t('footer_link_buy_sk') }</Link>
+                        </li>
+                        <li>
+                            <Link to="/">{ props.t('footer_link_privacy_policy') }</Link>
+                        </li>
+                    </ul>
+                    <div className='footer__community'>
+                        <h4>{ props.t('footer_community_title') }</h4>
+                        <p>{ props.t('footer_community_desc') }</p>
+                        <SocialNetworks />
+                    </div>
                 </div>
+                <div className='footer__content'>{ props.t('footer_text') }</div>
             </div>
-            <div className='footer__content'>{ props.t('footer_text') }</div>
-        </div>
-    </footer>
+        </footer>
+        <a className='cg-link' href="https://coinhunt.cc/coin/768028677" target="_blank" rel="noreferrer">
+            <img src="https://api.cnhnt.cc/public/getEmbedImage/768028677" alt="Shika Kage" />
+        </a>
+    </>
 ));
